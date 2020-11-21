53d199a3e03665036918817e94544c347314de96
houssni ouchad <50273874+ouchad97@users.noreply.github.com>
2020-11-21 23:36:33 +0100

    gestion des apprenants Csharp

---
+++ Scolaire/Scolaire/Form1.Designer.cs
@@ -0,0 +1,311 @@
+﻿namespace Scolaire
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.nom = new System.Windows.Forms.TextBox();
+            this.prenom = new System.Windows.Forms.TextBox();
+            this.datenaissance = new System.Windows.Forms.DateTimePicker();
+            this.tel = new System.Windows.Forms.TextBox();
+            this.email = new System.Windows.Forms.TextBox();
+            this.pays = new System.Windows.Forms.ComboBox();
+            this.ville = new System.Windows.Forms.ComboBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.errorProviderTel = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProviderEmail = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProviderNom = new System.Windows.Forms.ErrorProvider(this.components);
+            this.errorProviderPrenom = new System.Windows.Forms.ErrorProvider(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderTel)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderEmail)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderNom)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderPrenom)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // nom
+            // 
+            this.nom.Location = new System.Drawing.Point(179, 57);
+            this.nom.Name = "nom";
+            this.nom.Size = new System.Drawing.Size(215, 22);
+            this.nom.TabIndex = 0;
+            this.nom.Validating += new System.ComponentModel.CancelEventHandler(this.nom_Validating);
+            // 
+            // prenom
+            // 
+            this.prenom.Location = new System.Drawing.Point(620, 57);
+            this.prenom.Name = "prenom";
+            this.prenom.Size = new System.Drawing.Size(215, 22);
+            this.prenom.TabIndex = 1;
+            this.prenom.Validating += new System.ComponentModel.CancelEventHandler(this.prenom_Validating);
+            // 
+            // datenaissance
+            // 
+            this.datenaissance.Location = new System.Drawing.Point(179, 153);
+            this.datenaissance.Name = "datenaissance";
+            this.datenaissance.Size = new System.Drawing.Size(215, 22);
+            this.datenaissance.TabIndex = 2;
+            // 
+            // tel
+            // 
+            this.tel.Location = new System.Drawing.Point(620, 153);
+            this.tel.Name = "tel";
+            this.tel.Size = new System.Drawing.Size(215, 22);
+            this.tel.TabIndex = 3;
+            this.tel.Validating += new System.ComponentModel.CancelEventHandler(this.tel_Validating);
+            // 
+            // email
+            // 
+            this.email.Location = new System.Drawing.Point(179, 238);
+            this.email.Name = "email";
+            this.email.Size = new System.Drawing.Size(287, 22);
+            this.email.TabIndex = 4;
+            this.email.Validating += new System.ComponentModel.CancelEventHandler(this.email_Validating);
+            // 
+            // pays
+            // 
+            this.pays.FormattingEnabled = true;
+            this.pays.Location = new System.Drawing.Point(179, 320);
+            this.pays.Name = "pays";
+            this.pays.Size = new System.Drawing.Size(190, 24);
+            this.pays.TabIndex = 5;
+            this.pays.SelectedIndexChanged += new System.EventHandler(this.pays_SelectedIndexChanged);
+            // 
+            // ville
+            // 
+            this.ville.FormattingEnabled = true;
+            this.ville.Location = new System.Drawing.Point(620, 320);
+            this.ville.Name = "ville";
+            this.ville.Size = new System.Drawing.Size(190, 24);
+            this.ville.TabIndex = 6;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(255, 408);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(54, 21);
+            this.radioButton1.TabIndex = 7;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "JEE";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(420, 408);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(46, 21);
+            this.radioButton2.TabIndex = 8;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "C#";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(572, 408);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(155, 21);
+            this.radioButton3.TabIndex = 9;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Back-end Front-end";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(54, 60);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(37, 17);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Nom";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(528, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 17);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Prenom";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(54, 158);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(108, 17);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Date Naissance";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(54, 243);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 17);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Email";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(528, 158);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(76, 17);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Téléphone";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(54, 327);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(39, 17);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "Pays";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(546, 327);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(34, 17);
+            this.label7.TabIndex = 17;
+            this.label7.Text = "Ville";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(54, 410);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(69, 17);
+            this.label8.TabIndex = 18;
+            this.label8.Text = "Spécialité";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(399, 475);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(100, 31);
+            this.button1.TabIndex = 19;
+            this.button1.Text = "Valider";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // errorProviderTel
+            // 
+            this.errorProviderTel.ContainerControl = this;
+            // 
+            // errorProviderEmail
+            // 
+            this.errorProviderEmail.ContainerControl = this;
+            // 
+            // errorProviderNom
+            // 
+            this.errorProviderNom.ContainerControl = this;
+            // 
+            // errorProviderPrenom
+            // 
+            this.errorProviderPrenom.ContainerControl = this;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(901, 530);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.ville);
+            this.Controls.Add(this.pays);
+            this.Controls.Add(this.email);
+            this.Controls.Add(this.tel);
+            this.Controls.Add(this.datenaissance);
+            this.Controls.Add(this.prenom);
+            this.Controls.Add(this.nom);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderTel)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderEmail)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderNom)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProviderPrenom)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox nom;
+        private System.Windows.Forms.TextBox prenom;
+        private System.Windows.Forms.DateTimePicker datenaissance;
+        private System.Windows.Forms.TextBox tel;
+        private System.Windows.Forms.TextBox email;
+        private System.Windows.Forms.ComboBox pays;
+        private System.Windows.Forms.ComboBox ville;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ErrorProvider errorProviderTel;
+        private System.Windows.Forms.ErrorProvider errorProviderEmail;
+        private System.Windows.Forms.ErrorProvider errorProviderNom;
+        private System.Windows.Forms.ErrorProvider errorProviderPrenom;
+    }
+}
+
---
+++ Scolaire/Scolaire/Form1.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Scolaire
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+       
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //combobox pays
+            pays.Text = "sélectionnez votre pays";
+            pays.Items.Add("Maroc");
+            pays.Items.Add("France");
+            pays.Items.Add("Espagne");
+
+            ville.Enabled = false; 
+            ville.Text = "sélectionnez votre ville";
+        }
+
+        private void pays_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //combobox ville
+            ville.Enabled = true;
+            if (pays.SelectedItem.ToString() == "Maroc") {
+                ville.Items.Clear();
+                ville.Items.Add("Rabat");
+                ville.Items.Add("Casablanca");
+                ville.Items.Add("Safi");
+                ville.Items.Add("Fes");
+                ville.Items.Add("Agadir");
+            }
+            else{
+                if (pays.SelectedItem.ToString() == "France") {
+                    ville.Items.Clear();
+                    ville.Items.Add("Marseille");
+                    ville.Items.Add("Bordeaux");
+                    ville.Items.Add("Strasbourg");
+                    ville.Items.Add("Lyon");
+                    ville.Items.Add("Paris");
+                }
+
+                if (pays.SelectedItem.ToString() == "Espagne") {
+                    ville.Items.Clear();
+                    ville.Items.Add("Barcelona");
+                    ville.Items.Add("Seville");
+                    ville.Items.Add("Madrid");
+                    ville.Items.Add("Bilbao");
+                    ville.Items.Add("Granada");
+                }
+            }
+
+        }
+
+        private void prenom_Validating(object sender, CancelEventArgs e)
+        {
+            System.Text.RegularExpressions.Regex rpreNom = new System.Text.RegularExpressions.Regex(@"^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$");
+
+            if (string.IsNullOrEmpty(prenom.Text))
+            {
+                errorProviderNom.SetError(prenom, "Prenom required!");
+            }
+            else if (!rpreNom.IsMatch(prenom.Text))
+            {
+                errorProviderNom.SetError(prenom, "PrenomInvalid");
+            }
+            else
+            {
+                errorProviderNom.SetError(prenom, null);
+            }
+        }
+        private void nom_Validating(object sender, CancelEventArgs e)
+        {
+            System.Text.RegularExpressions.Regex rNom = new System.Text.RegularExpressions.Regex(@"^[a-zA-Z]+(([',. -][a-zA-Z ])?[a-zA-Z]*)*$");
+
+            if (string.IsNullOrEmpty(nom.Text))
+            {
+                errorProviderNom.SetError(nom, "Nom required!");
+            }
+            else if (!rNom.IsMatch(nom.Text))
+            {
+                errorProviderNom.SetError(nom, "Nom Invalid");
+            }
+            else {
+                errorProviderNom.SetError(nom, null);
+            }
+        }
+        private void email_Validating(object sender, CancelEventArgs e)
+        {
+                System.Text.RegularExpressions.Regex rEMail = new System.Text.RegularExpressions.Regex(@"^[a-zA-Z][\w\.-]*[a-zA-Z0-9]@[a-zA-Z0-9][\w\.-]*[a-zA-Z0-9]\.[a-zA-Z][a-zA-Z\.]*[a-zA-Z]$");
+
+
+                if (string.IsNullOrEmpty(email.Text))
+                {
+                    errorProviderEmail.SetError(email, "Email required!");
+                }
+                    else if (!rEMail.IsMatch(email.Text)){
+                        errorProviderEmail.SetError(email,"Email Invalid");
+                    }
+                    else {
+                        errorProviderEmail.SetError(email, null);
+                    } 
+        }
+
+        private void tel_Validating(object sender, CancelEventArgs e)
+        {  System.Text.RegularExpressions.Regex rTEL = new System.Text.RegularExpressions.Regex(@"^[0-7]{2}[0-9]{8}$");
+
+            if (string.IsNullOrEmpty(tel.Text)){
+                errorProviderTel.SetError(tel, "Phone required!");
+                }
+            else if (!rTEL.IsMatch(tel.Text)){
+                    errorProviderTel.SetError(tel, "Phone Invalid");
+                }
+                else{
+                    errorProviderTel.SetError(tel, null);
+                    }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (Control control in this.Controls)
+            {
+                control.Focus();
+                if (!Validate())
+                {
+                    DialogResult = DialogResult.None;
+                    return;
+                }
+            }
+        }
+
+       
+    }
+}
